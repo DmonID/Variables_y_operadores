8f1a70956dd330f304ead75ae78e98e0627a0fc2
Fernando Gorostieta Vargas <AL03065928@tecmilenio.mx>
2024-02-16 19:42:48 -0500

    Update

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        System.out.println("Suma: " + (x + y));
+        System.out.println("Resta: " + (x - y));
+        System.out.println("Multiplicación: " + (x * y));
+        System.out.println("División: " + (x / y));
+        System.out.println("Módulo: " + (x % y));
+    }
+}
